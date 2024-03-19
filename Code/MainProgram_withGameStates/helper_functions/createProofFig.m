032fb4f62767bc92cc7c91e7c217d3ec8bdb9eca
Akyazı <d.akyazi@tue.nl>
2024-03-19 17:03:57 +0100

    Added game state UI to main

---
+++ Code/MainProgram_withGameStates/helper_functions/createProofFig.m
@@ -0,0 +1,8 @@
+function [f, t] = createProofFig()
+    % Create figure at right top for BOOP and Last Touch Proof with
+    % tiledlayout
+    % To plot to specific axes (subplot): nexttile(t,1) OR nexttile(t,2)
+    f = figure('Name','Assistant Referee Proof','NumberTitle','off');
+    f.Position = [260 180 1050 460];
+    t = tiledlayout(f,1,2);
+end
